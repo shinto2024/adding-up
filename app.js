1dae7dfa0f5c2b6277545a5c5aec11d0d78a40a0
shinto2024 <154789190+shinto2024@users.noreply.github.com>
2024-02-23 20:09:12 +0900

    app.js の実装の途中まで

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 'use strict';
+const fs = require('node:fs');
+const readline = require('node:readline');
+const rs = fs.createReadStream('./popu-pref.csv')
+const rl = readline.createInterface({ input: rs});
+rl.on('line', lineString => {
+  console.log(lineString);
+})
